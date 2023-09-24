6f598bfb904271e54a6915f51e4d0f0cd0395f3f
bitress <cyannejustinl.vega12@gmail.com>
2023-09-24 12:56:34 +0800

    uplood

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+var bitress = {
+    Utils : {}
+}
+
+
+bitress.Utils.clock = function ()
+{
+
+    const currentDate = new Date();
+
+    const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+    ];
+
+    const daysOfWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"
+    ];
+
+    const dayOfWeek = daysOfWeek[currentDate.getDay()];
+    const month = months[currentDate.getMonth()];
+    const day = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    const ampm = hours >= 12 ? "PM" : "AM";
+
+    const formattedTime = `${dayOfWeek}, ${month} ${day}, ${year}, ${hours}:${minutes}:${seconds} ${ampm}`;
+
+
+
+    const pstTIme = document.querySelector("#pst-time a")
+    pstTIme.textContent = formattedTime
+
+}
+
+
+bitress.Utils.clock();
+setInterval(bitress.Utils.clock, 1000)
